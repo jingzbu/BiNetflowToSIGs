c7d888e1b25c89969f29e1899aa4813e9f7ee4df
jingzbu <jingzbu@gmail.com>
2015-07-20 19:04:36 -0400

    add data and test output

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+./binetflowtosigs -w 10 -s capture20110816.binetflow.10s.sigs
+
+./binetflowtosigs -w 20 -s capture20110816.binetflow.20s.sigs
+
+./binetflowtosigs -w 50 -s capture20110816.binetflow.50s.sigs
+
+./binetflowtosigs -w 100 -s capture20110816.binetflow.100s.sigs
